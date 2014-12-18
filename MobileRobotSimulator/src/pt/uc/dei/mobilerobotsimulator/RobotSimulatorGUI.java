e13f542c383d5ebf9a712751dd480b39f0d0b937
Karin Piskur <piskur@student.dei.uc.pt>
2014-12-18 01:34:31 +0000

    gui update

--- MobileRobotSimulator/src/pt/uc/dei/mobilerobotsimulator/RobotSimulatorGUI.java
+++ MobileRobotSimulator/src/pt/uc/dei/mobilerobotsimulator/RobotSimulatorGUI.java
@@ -6,10 +6,15 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.FlowLayout;
 
 public class RobotSimulatorGUI extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textField;
 
 	/**
 	 * Launch the application.
@@ -31,12 +36,28 @@ public class RobotSimulatorGUI extends JFrame {
 	 * Create the frame.
 	 */
 	public RobotSimulatorGUI() {
+		setTitle("Mobile Robot Simulator");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		
+		JPanel panel = new JPanel();
+		FlowLayout flowLayout = (FlowLayout) panel.getLayout();
+		flowLayout.setAlignOnBaseline(true);
+		flowLayout.setAlignment(FlowLayout.LEFT);
+		contentPane.add(panel, BorderLayout.CENTER);
+		
+		JLabel lblColor = new JLabel("Select configuration file:");
+		lblColor.setHorizontalAlignment(SwingConstants.CENTER);
+		panel.add(lblColor);
+		
+		textField = new JTextField();
+		textField.setHorizontalAlignment(SwingConstants.LEFT);
+		panel.add(textField);
+		textField.setColumns(10);
 	}
 
 }
